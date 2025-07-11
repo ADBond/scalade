211caafa5210d18bc559488445bcfabd6a0f7fef
ADBond <48208438+ADBond@users.noreply.github.com>
2025-07-11 16:19:36 +0100

    some info on the game itself

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", async () => {
   const game = new Game(['Andy', 'Randy1', 'Randy2']);
   await renderState(game.getGameStateForUI());
 });
+
+const helpButton = document.getElementById('help-button')!;
+const helpModal = document.getElementById('help-modal')!;
+const helpClose = document.getElementById('help-close')!;
+
+helpButton.addEventListener('click', () => {
+  helpModal.style.display = 'block';
+});
+
+helpClose.addEventListener('click', () => {
+  helpModal.style.display = 'none';
+});
+
+window.addEventListener('click', (e: MouseEvent) => {
+  if (e.target === helpModal) {
+    helpModal.style.display = 'none';
+  }
+});
