7abe6d4db59ae64e649251fb4352eb3896459696
ADBond <48208438+ADBond@users.noreply.github.com>
2025-08-24 20:05:26 +0100

    choose capping in ui

--- src/interface/game.ts
+++ src/interface/game.ts
@@ -1,11 +1,11 @@
 import { Game } from "../game/game";
-import { GameMode } from "../game/gamestate";
+import { GameMode, BonusCapping } from "../game/gamestate";
 
 let game: Game;
 
-export function newGame(gameMode: GameMode, escalations: number): void {
+export function newGame(gameMode: GameMode, escalations: number, capping: BonusCapping): void {
     // TODO: use names for display, overriding comp1 comp2 etc which should be internal
-    game = new Game(['Andy', 'Randy1', 'Randy2'], gameMode, escalations);
+    game = new Game(['Andy', 'Randy1', 'Randy2'], gameMode, escalations, capping);
 }
 
 export function getGame(): Game {
