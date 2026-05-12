9da5ae56e41d73a44d04228f7fc1d8adb2d9ba84
ADBond <48208438+ADBond@users.noreply.github.com>
2026-05-12 22:29:10 +0100

    local docker build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import { execSync } from 'child_process'
 
-const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+let commitHash = 'dev';
+try {
+  commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+} catch {
+  // git not available
+}
 
 export default defineConfig({
   base: '/scalade/',
